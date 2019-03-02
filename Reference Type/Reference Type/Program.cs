5d012cc20bf2a62d2095029d1a5aaf8922ad006b
최동환 <nimeol@hotmail.com>
2019-03-02 14:12:29 +0900

    Reference Type예제 26page

---
+++ Reference Type/Reference Type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reference_Type
+{
+
+    class Rtype {
+        public object value;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rtype a = new Rtype();
+            Rtype b = new Rtype();
+
+            a.value = 1;    b = a;
+            Console.WriteLine(a.value); Console.WriteLine(b.value);
+
+            b.value = 2;
+            Console.WriteLine(a.value); Console.WriteLine(b.value);
+        }
+    }
+}
--- null연산자실습/null연산자실습/Program.cs
+++ null연산자실습/null연산자실습/Program.cs
@@ -4,20 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace null연산자실습
+namespace null연산자실습 //22page
 {
     class Program
     {
         static void Main()
         {
+            //Nullable Type, a 가 NULL 을 허용한다는 의미   원칙상 int는 null를 받아 들이지 않는다.
             int? a = null;
+            //int b = a.Value;     // a.Value 값 자체가 null 이므로 int? b 도 오류 
+            //Console.WriteLine("Value of b is {0}", b); 
+
 
             int b;
             if (a.HasValue) b = a.Value;
             else b = 0;
             Console.WriteLine("Value of b is{0}", b);
 
-            int c = a ?? 0;
+            int c = a ?? 0; //?? 은 null결합 연산자
             Console.WriteLine("Value of c is {0}", c);
             Console.ReadLine();
         }
