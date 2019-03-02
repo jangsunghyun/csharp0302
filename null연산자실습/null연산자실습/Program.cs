c5a88bf00bc65fcd499047354ba72bb998406545
최동환 <nimeol@hotmail.com>
2019-03-02 14:03:32 +0900

    Null 연산자실습

---
+++ null연산자실습/null연산자실습/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null연산자실습
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? a = null;
+
+            int b;
+            if (a.HasValue) b = a.Value;
+            else b = 0;
+            Console.WriteLine("Value of b is{0}", b);
+
+            int c = a ?? 0;
+            Console.WriteLine("Value of c is {0}", c);
+            Console.ReadLine();
+        }
+    }
+}
