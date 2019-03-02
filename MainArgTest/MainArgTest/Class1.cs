1eaa787936856d935cf7b58185cd3fb4c2724cfd
최동환 <nimeol@hotmail.com>
2019-03-02 13:05:53 +0900

    TypeCase1

---
+++ MainArgTest/MainArgTest/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MainArgTest
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+        }
+    }
+}
---
+++ TypeCast1/TypeCast1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace TypeCast1
+{
+    class TypeCast1
+    {
+        static void Main()
+        {
+            Console.WriteLine(default(int));
+
+            int iSize = sizeof(int);
+            Console.WriteLine("int형 바이트길이 : {0}", iSize);
+
+            Type myType1 = typeof(int);
+            Console.WriteLine("typeof(int) : {0}", myType1);
+
+            int i = 10;
+            Type myType2 = i.GetType();
+            Console.WriteLine("i.GetType() : {0}", myType2);
+
+            int j = 99;
+            Console.WriteLine("Size of j : {0}", Marshal.SizeOf(j));   //변수들의 사이즈
+        }
+    }
+}
