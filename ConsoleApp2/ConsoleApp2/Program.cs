12356b65c2f2d765026fd0521951b514bd5c66fd
최동환 <nimeol@hotmail.com>
2019-03-03 14:37:25 +0900

    ,ref, out

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Swap(out int a, out int b)
+        {
+            int tmp = a; a = b; b = tmp;
+        }
+
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //int b = 20;
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+
+            //ref 원복 주소값을 넘긴다. 
+            //ref는 기본 변수값에 초기화 값이 들어가 있어야 한다.
+            //Swap(ref a, ref b);
+
+            //out은 변수 선언만 되어 있어야 한다. out도 주소를 넘긴다.
+            //out은 초기화 하면 안됨
+            int a, b;
+            Swap (out a, out b)
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+    }
+}
