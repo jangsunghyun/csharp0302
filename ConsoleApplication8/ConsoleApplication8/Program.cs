142f8c7497964870847184fa9eb1234affccd2e4
최동환 <nimeol@hotmail.com>
2019-03-03 13:45:12 +0900

    소멸자 패턴 page 83

--- ConsoleApplication8/ConsoleApplication8/Program.cs
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
 namespace ConsoleApplication8
-{
-    class MyBaseClass
-    {
+{   //부모의 부모는 object
+    class MyBaseClass : Object
+    {           
         public MyBaseClass()
         {
             Console.WriteLine(">>> MyBaseClasee()");
@@ -17,15 +17,20 @@ namespace ConsoleApplication8
 
     class MyClass : MyBaseClass
     {
-        public MyClass()
+        //base 생략되어 있다.
+        public MyClass(): base()
         {
+            //생성자의 첫번째 라인부터 부모생성자 불러온다. base 
             /////////////////////////////
             Console.WriteLine(">>> MyClass()");
         }
+        // base(i)는 MyClass(int i)이거 실행되기 전에  base(i) 실행해 준다.. base class 부모 클래스
+        //base(i) 부모 class
         public MyClass(int i) : base(i)
         {
             Console.WriteLine(">>> MyClass(int i)");
         }
+        //: 없으면 base 먼저읽는다. 
         public MyClass(int i, int j) 
         {
             Console.WriteLine(">>> MyClass(int i, int j)");
@@ -34,7 +39,7 @@ namespace ConsoleApplication8
         {
             Console.WriteLine(">>> MyClass(int i, int j, int k)");
         }
-        public MyClass(int i, int j, int k, int l) : base(i)
+        public MyClass(int i, int j, int k, int l) : this(i, j)
         {
             Console.WriteLine(">>> MyClass(int i, int j, int k, int l)");
         }
@@ -48,7 +53,7 @@ namespace ConsoleApplication8
             MyClass m1 = new MyClass();
             MyClass m2 = new MyClass(1);
             MyClass m3 = new MyClass(1,2);
-            MyClass m4 = new MyClass(1, 2,3);
+            MyClass m4 = new MyClass(1, 2, 3);
             MyClass m5 = new MyClass(1, 2, 3,4);
         }
     }
---
+++ IDisposable/IDisposable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Garbage : IDisposable {
+
+    private bool isDispose = false;
+    private string name;
+
+    public Garbage(string name) {
+        this.name = name;
+        Console.WriteLine("{0}객체 생성됨...", this.name);
+    }
+
+    //소멸자
+    ~Garbage() {
+        //sDispose 안되어 있으면 Dispose() 호출
+        if (!isDispose) { Dispose(); }
+    }
+
+    public void Dispose() {
+        //Dispose() 되었다. true 
+        isDispose = true;
+
+        Console.WriteLine("{0}객체 리소스 해제 OK...", name);
+
+        //Dispose() 후 소멸자 안부르게
+        GC.SuppressFinalize(this);
+    }
+}
+
+
+class GagageTest1 {
+    static void Main() {
+        Garbage g1 = new Garbage("1번객체");
+        Garbage g2 = new Garbage("2번객체");
+        Garbage g3 = new Garbage("3번객체");
+        Garbage g4 = new Garbage("4번객체");
+
+        g1.Dispose();
+
+        //g2 소멸자 안부르게
+        GC.SuppressFinalize(g2);
+
+        //나머지는 언제 소멸될지 모른다. 그러나 윈도우 10은 프로그램 끝나자마자 바로 소멸된다. 
+    }
+}
