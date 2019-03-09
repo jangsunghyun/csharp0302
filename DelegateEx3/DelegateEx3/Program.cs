e2a8e8913547744f5b8a2132999f86971aeba25e
최동환 <nimeol@hotmail.com>
2019-03-09 14:20:27 +0900

    delegate ex 3

---
+++ DelegateEx3/DelegateEx3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelegateEx3
+{
+    class Program
+    {
+        delegate void Deli(string s);
+
+        static void Main(string[] args)
+        {
+            Deli d0 = (v) => Console.WriteLine(v);
+            d0.Invoke("OJC");
+
+            Deli d1 = new Deli(d);
+            d1.Invoke("OJC");
+
+            Deli d2 = d;
+            d2.Invoke("OJC");
+        }
+
+        static void d(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
