c72ae51b20c15621be20efaee2b90ca50aeea3c7
최동환 <nimeol@hotmail.com>
2019-03-09 17:43:18 +0900

    이벤트 앤들러

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    //이벤트 발생시 넘길 데이터 (System.EventArgs에서 파생)
+    class EventPublisherArgs : System.EventArgs
+    {
+        public string myEventData;
+        public EventPublisherArgs(string myEventData)
+        {
+            this.myEventData = myEventData;
+        }
+    }
+
+    //이벤트 게시자 클래스
+    class EventPublisher
+    {
+        //이벤트 처리를 위한 델리게이트 정의
+        //public delegate void MyEventHandler(object sender, EventPublisherArgs e);
+        public event EventHandler MyEvent;    //이벤트 정의
+
+        public void Do()
+        {
+            //이벤트 가입자가 있는지 확인
+            if (MyEvent != null)
+            {
+                EventPublisherArgs args = new EventPublisherArgs("데이터");
+                MyEvent(this, args);    //이벤트 발생
+            }
+        }
+    }
+
+    //구독자 클랙스
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher p = new EventPublisher();
+            //p.MyEvent += new EventPublisher.MyEventHandler(doAction);
+            //p.MyEvent += (sender, e) =>
+            //{
+            //    Console.WriteLine("Myevent 라는 이벤트 발생");
+            //};
+            p.MyEvent += new EventHandler(doAction);
+
+            p.Do();
+        }
+
+        //MyEvent 이벤트가 발생하면 호출되는 메서드
+        //EventPublisherArgs 가 발생시 넘어오는 데이터이다.
+        static void doAction(object sender, EventArgs e)
+        {
+            Console.WriteLine("MyEvent 라는 이벤트 발생...");
+            Console.WriteLine("이벤트 매개변수 : " + ((EventPublisherArgs)e).myEventData);
+        }
+    }
+
+}
---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication11
+{
+    class EventPublisher
+    {
+        public delegate void MyEventHandler();
+        public event MyEventHandler MyEvent;
+        public void Do()
+        {
+            MyEvent?.Invoke();
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            EventPublisher p = new EventPublisher();
+            //구독자의 이벤트 처리 메소드를 등록
+            //이벤트를 발생하면 호출될 메소드를 정의, 여기서는 doAction 메소드 호출
+            //MyEvent 라는 이벤트에 이벤트핸들러인 델리게이트를 통해 메소드를 등록
+            //MyEvent 라는 이벤트가 발생하면 델리게이트가 참조하는 메소드 doAction() 실행
+            p.MyEvent += new EventPublisher.MyEventHandler(doAction);
+            //테스트를 위해 do() 메소드를 호출하여 이벤트 발생
+            //실제 윈폼등에서는 버튼등을 클릭하면 자동으로 이벤트가 발생하지만
+            //콘솔 프로그램이라서 메소드를 호출하는 것이다.
+            p.Do();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        static void doAction()
+        {
+            Console.WriteLine("MyEvent 라는 이벤트 발생...");
+        }
+
+    }
+}
