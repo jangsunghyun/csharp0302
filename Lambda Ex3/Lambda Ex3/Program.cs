a6903295ae5bd5a6962ae85502010fcaf608dc91
최동환 <nimeol@hotmail.com>
2019-03-09 18:21:34 +0900

    델리게이트 Action

--- Lambda Ex3/Lambda Ex3/Program.cs
+++ Lambda Ex3/Lambda Ex3/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Lambda_Ex3
 {
+
     class Program
     {
+        delegate int Sum(int[] arg);
         static void Main(string[] args)
         {
+            Sum sumdeli = (arg) =>
+            {
+                int mySum = 0;
+                foreach (int i in arg)
+                    mySum += i;
+                return mySum;
+            };
+            int sum = sumdeli(new int[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine("1+2+3+4+5=" + sum);
         }
+
     }
 }
