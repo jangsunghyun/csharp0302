d62aca03c6962e0e95f02fe994e657dfe0f5f695
최동환 <nimeol@hotmail.com>
2019-03-10 12:21:43 +0900

    server ex 1

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -23,17 +23,28 @@ namespace ConsoleApplication3
             
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(new ThreadStart(() => Sum(new int[] { 1, 10 })));
-            Thread t2 = new Thread(new ThreadStart(() => Sum(new int[]{11, 20 })));
-            Thread t3 = new Thread(new ThreadStart(() => Sum(new int[] { 21, 30 })));
-            Thread t4 = new Thread(new ThreadStart(() => Sum(new int[] { 31, 40 })));
-            Thread t5 = new Thread(new ThreadStart(() => Sum(new int[] { 41, 50 })));
+            Thread t1 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t2 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t3 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t4 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t5 = new Thread(new ParameterizedThreadStart(Sum));
 
-            t1.Start(); t2.Start(); t3.Start(); t4.Start(); t5.Start();
+            t1.Start(new int[] {1,10}); t2.Start(new int[] { 11, 20 });
+            t3.Start(new int[] { 21, 30 }); t4.Start(new int[] { 31, 40 }); t5.Start(new int[] { 41, 50 });
             t1.Join(); t2.Join(); t3.Join(); t4.Join(); t5.Join(); //join 독립 메소드..   
+
+
+            //Thread t1 = new Thread(new ThreadStart(() => Sum(new int[] { 1, 10 })));
+            //Thread t2 = new Thread(new ThreadStart(() => Sum(new int[]{11, 20 })));
+            //Thread t3 = new Thread(new ThreadStart(() => Sum(new int[] { 21, 30 })));
+            //Thread t4 = new Thread(new ThreadStart(() => Sum(new int[] { 31, 40 })));
+            //Thread t5 = new Thread(new ThreadStart(() => Sum(new int[] { 41, 50 })));
+
+            //t1.Start(); t2.Start(); t3.Start(); t4.Start(); t5.Start();
+            //t1.Join(); t2.Join(); t3.Join(); t4.Join(); t5.Join(); //join 독립 메소드..   
             //join 은 각 계산이 끝날때까지 기다렸다가 sum합을 구한다. 
 
-            Console.WriteLine(mysum);
+            Console.WriteLine("1부터 50까지의 합은 {0}::",mysum);
 
         }
     }
