2172181f9ab376761d366b8b94f4460c944e7a0e
최동환 <nimeol@hotmail.com>
2019-03-10 14:14:03 +0900

    MultiThreadSever ex

---
+++ MultiThreadSever/MultiThreadSever/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace MultiThreadSever
+{
+    class ClientHadleer
+    {
+        Socket socket = null;
+        NetworkStream stream = null;
+        StreamReader reader = null;
+        StreamWriter writer = null;
+
+        public ClientHadleer(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        public void chat()
+        {
+            //클라이언트의 데이터르 읽고 쓰기 위한 스트림을 만든다.
+            stream = new NetworkStream(socket);
+            Encoding encode = Encoding.GetEncoding("utf-8");
+
+            reader = new StreamReader(stream, encode);
+            writer = new StreamWriter(stream, encode) { AutoFlush = true };
+
+            while (true)
+            {
+                string str = reader.ReadLine();
+                Console.WriteLine(str);
+
+                writer.WriteLine(str);
+            }
+        }
+    }
+
+    class Sever
+    {
+        public static void Main()
+        {
+            TcpListener tcpListener = null;
+            Socket clientsocket = null;
+
+            try
+            {
+                //IP 주소를 나타내는 객체를 생성, TcpListener를 생성시 인자로 사용할려고
+                IPAddress ipAd = IPAddress.Parse("192.168.0.41");
+
+                //TcpListener Class를 이용하여 클라이언트의 연결을 받아 들인다. 
+                tcpListener = new TcpListener(ipAd, 5001);
+                tcpListener.Start();
+
+                //Client의 접속이 올때 까지 Block 되는 부분, 대개 이부분을 Thread로 만들어 보내 버린다
+                //백그라운드 Thread에 처리를 맡긴다. 
+                while (true)
+                {
+                    clientsocket = tcpListener.AcceptSocket();
+
+                    ClientHadleer cHandler = new ClientHadleer(clientsocket);
+                    Thread t = new Thread(new ThreadStart(cHandler.chat));
+                    t.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                clientsocket.Close();
+            }
+        }
+    }
+}
--- TcpClientTest/TcpClientTest/Program.cs
+++ TcpClientTest/TcpClientTest/Program.cs
@@ -20,15 +20,22 @@ namespace TcpClientTest
                 //LocalHost에 지정 포트로 TCP Connection을 생성하고 데이터를 송수신 하기              
                 //위한 스트림을 얻는다. 
                 client = new TcpClient();
-                client.Connect("localhost", 5001);
+                client.Connect("192.168.0.41", 5001);
                 NetworkStream stream = client.GetStream();
-                StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                Encoding encode = Encoding.GetEncoding("utf-8");
+
+                StreamReader reader = new StreamReader(stream, encode);
+                StreamWriter writer = new StreamWriter(stream, encode)
+                { AutoFlush = true };                
 
                 string dataToSend = Console.ReadLine();
 
                 while (true)
                 {
                     writer.WriteLine(dataToSend);
+                    string str = reader.ReadLine();
+                    Console.WriteLine(str);
 
                     if (dataToSend.IndexOf("<EOF>") > -1) break;
 
